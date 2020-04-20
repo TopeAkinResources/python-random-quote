2971f1cec3b13e5a0601403daecfe87274d737f8
Tayme tee <taymetee@Taymes-MacBook-Pro-2.local>
2020-04-20 02:12:21 +0100

    Got random quote bot working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def load_all():
-  print("Keep it logically awesome.")
+ #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  quotes_file = open("quotes.txt")
+  all_quotes = quotes_file.readlines()
+  quotes_file.close()
 
-  #print(quotes)
+  #print(all_quotes)
+  print(all_quotes[-1])
 
 if __name__== "__main__":
   load_all()
