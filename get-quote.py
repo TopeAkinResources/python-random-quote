1196069d81f1a395334a0d9a7633a8e62cc08dcd
Tayme tee <taymetee@Taymes-MacBook-Pro-2.local>
2020-04-20 14:47:02 +0100

    Made 2 custom changes

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,26 @@
+import random
 def load_all():
  #print("Keep it logically awesome.")
 
   quotes_file = open("quotes.txt")
-  all_quotes = quotes_file.readlines()
+  quotes = quotes_file.readlines()
   quotes_file.close()
 
+  number_of_lines = len(quotes)
+
+  #print(number_of_lines -1)
+
+  #last = 13
+  rnd = random.randint(0, number_of_lines-1)
+  rnd2 = random.randint(0, number_of_lines-1)
+
+  #last = len(quotes_file) -1
+  #print(quotes_file[rnd])
   #print(all_quotes)
-  print(all_quotes[-1])
+  #print(quotes[-1])	
+
+  print(quotes[rnd], end = '')
+  print(quotes[rnd2], end ='')
 
 if __name__== "__main__":
   load_all()
